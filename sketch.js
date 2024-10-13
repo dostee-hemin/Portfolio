0d52bb3d8fbb29925ea76cef30d61319fb86de57
dostee-hemin <dostee.hemin@gmail.com>
2024-10-13 23:30:55 +0300

    Removed Pixel Density Calculation

--- sketch.js
+++ sketch.js
@@ -21,7 +21,6 @@ function setup() {
   isMobileDevice = displayWidth < displayHeight;
   
   windowResized();
-  if(isMobileDevice) pixelDensity(1);
   setupTetrisPattern();
 
   animator = new Animator();
