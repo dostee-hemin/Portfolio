f6ec6cae9f72d5cb0700023e0a3178576412be41
dostee-hemin <dostee.hemin@gmail.com>
2025-02-12 15:07:37 +0300

    Fixed Mobile Refresh Bug

--- sketch.js
+++ sketch.js
@@ -194,3 +194,5 @@ function distSq(x1, y1, x2, y2) {
   return (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1);
 }
 
+/// Add these lines below sketch to prevent scrolling on mobile
+function touchMoved() {return false;}
